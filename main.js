2731eebb242257a4431b54fd58c0357d231e35ee
Alena <elenila_skinef@mail.ru>
2023-02-20 18:10:48 +0300

    added    menu__popup for header

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 let menuButton = document.querySelector('.menu__button-burger');
-let menu = document.querySelector('.menu');
+let menuPopup = document.querySelector('#menu-popup');
+let body = document.body;
+let menu = document.querySelector('#menu-header').cloneNode(1);
+let links = Array.from(menu.children);
 
 menuButton.addEventListener('click', function() {
-  menuButton.classList.toggle('active');
+  menuPopup.classList.toggle("open");
+  menuButton.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderMenuPopup();
 });
+
+function renderMenuPopup() {
+  menuPopup.appendChild(menu);
+}
+
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+  menuPopup.classList.remove("open");
+  menuButton.classList.remove("active");
+  body.classList.remove("noscroll");
+}
